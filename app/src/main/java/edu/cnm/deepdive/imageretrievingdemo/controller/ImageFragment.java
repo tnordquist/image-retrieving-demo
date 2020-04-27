b4708dcca3a8686fe14137e54daf40aaebf761f4
Todd A Nordquist <toddnordquist144@gmail.com>
2020-04-27 07:20:47 -0600

    Added clickable spinner list
    Spinner contains a list of a few of the available animals. The list was hand written as an array in the strings xml resource file. The animals listed display the appropriate image when clicked on.

--- app/src/main/java/edu/cnm/deepdive/imageretrievingdemo/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/imageretrievingdemo/controller/ImageFragment.java
@@ -8,6 +8,11 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -16,16 +21,30 @@ import edu.cnm.deepdive.imageretrievingdemo.R;
 import edu.cnm.deepdive.imageretrievingdemo.viewmodel.MainViewModel;
 import java.util.Objects;
 
-public class ImageFragment extends Fragment {
+public class ImageFragment extends Fragment implements OnItemSelectedListener {
 
   private WebView contentView;
   private MainViewModel viewModel;
-  String url;
+
+  private Spinner spinner;
+  private SpinnerAdapter spinnerAdapter;
+  private AdapterView.OnItemSelectedListener itemSelected;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
     setupWebView(root);
+    spinner = root.findViewById(R.id.animals_spinner);
+    spinner.setOnItemSelectedListener(this);
+
+    // Create an ArrayAdapter using the string array and a default spinner layout
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+        Objects.requireNonNull(getContext()),
+        R.array.animals_array, R.layout.support_simple_spinner_dropdown_item);
+    // Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+    spinner.setAdapter(adapter);
     return root;
   }
 
@@ -35,9 +54,6 @@ public class ImageFragment extends Fragment {
     assert getParentFragment() != null;
     viewModel = new ViewModelProvider((Objects.requireNonNull(getActivity())))
         .get(MainViewModel.class);
-
-    viewModel.getAnimals().observe(getViewLifecycleOwner(),
-        (animals -> contentView.loadUrl(animals.get(24).getUrl())));
   }
 
   private void setupWebView(View root) {
@@ -49,7 +65,7 @@ public class ImageFragment extends Fragment {
       }
     });
     WebSettings settings = contentView.getSettings();
-    settings.setJavaScriptEnabled(true);
+    settings.setJavaScriptEnabled(false);
     settings.setSupportZoom(true);
     settings.setBuiltInZoomControls(true);
     settings.setDisplayZoomControls(false);
@@ -57,5 +73,17 @@ public class ImageFragment extends Fragment {
     settings.setLoadWithOverviewMode(true);
   }
 
+  @Override
+  public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+    viewModel.getAnimals().observe(getViewLifecycleOwner(),
+        (animals -> contentView.loadUrl(animals.get(pos).getUrl())));
+
+  }
+
+  @Override
+  public void onNothingSelected(AdapterView<?> adapterView) {
+    viewModel.getAnimals().observe(getViewLifecycleOwner(),
+        (animals -> contentView.loadUrl(animals.get(0).getUrl())));
+  }
 }
 
